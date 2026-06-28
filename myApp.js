3ec30e4f173b159a8fae8d8aa140d4ed8e1c55c2
Flouzum <mikhail.mmoskvitin06@gmail.com>
2026-06-28 19:47:49 +0800

    Update myApp.js
    Signed-off-by: Flouzum <mikhail.mmoskvitin06@gmail.com>

--- myApp.js
+++ myApp.js
@@ -24,4 +24,11 @@ app.get('/json', function(req, res) {
     res.json({ message: message });
 });
 
+app.get('/now', function(req, res, next) {
+    req.time = new Date().toString();
+    next();
+}, function(req, res) {
+    res.json({ time: req.time });
+});
+
 module.exports = app;
